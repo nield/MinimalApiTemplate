1bf55448415ba18b6d765963d8304609f1989e5f
Niel de Wet <v-niel.wet@d360.com>
2023-05-21 11:39:42 +0200

    feat(Sdk): Add Nswag generated Sdk

---
+++ src/MinimalApiTemplate.Sdk/StartupExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MinimalApiTemplate.Sdk;
+public static class StartupExtensions
+{
+    public static void SetupMinimalApi(this IServiceCollection services, Uri baseAddress)
+    {
+        services.AddHttpClient<IMinimalApiTemplateV1Service, MinimalApiTemplateV1Service>("minimalApiClient", config =>
+        {
+            config.BaseAddress = baseAddress;
+            config.Timeout = TimeSpan.FromSeconds(30);
+        });
+    }
+}
