d640305bff50d6b94539440af042f2421014133d
Niel de Wet <v-niel.wet@d360.com>
2023-11-28 14:30:24 +0200

    feat: change global exceptions to dot net 8 exception handler

--- src/MinimalApiTemplate.Api/Endpoints/V1/TodoItems/GetTodoItemsWithPaginationEndpoint.cs
+++ src/MinimalApiTemplate.Api/Endpoints/V1/TodoItems/GetTodoItemsWithPaginationEndpoint.cs
@@ -1,10 +1,11 @@
-﻿using MinimalApiTemplate.Api.Models.V1.Responses;
+﻿using MinimalApiTemplate.Api.Models.V1.Requests;
+using MinimalApiTemplate.Api.Models.V1.Responses;
 using MinimalApiTemplate.Application.Features.TodoItems.Queries.GetTodoItemsWithPagination;
 
 namespace MinimalApiTemplate.Api.Endpoints.V1.TodoItems;
 
 public class GetTodoItemsWithPaginationEndpoint : BaseEndpoint, 
-    IEndpoint<PaginatedListResponse<GetToDoItemsResponse>, GetTodoItemsWithPaginationQuery, CancellationToken>
+    IEndpoint<PaginatedListResponse<GetToDoItemsResponse>, GetTodoItemsWithPaginationRequest, CancellationToken>
 {
     public GetTodoItemsWithPaginationEndpoint(ISender sender, IMapper mapper)
         : base(sender, mapper)
@@ -16,13 +17,20 @@ public class GetTodoItemsWithPaginationEndpoint : BaseEndpoint,
     {
         app.ToDoItemRoute()
             .MapGet("",                        
-                ([AsParameters]GetTodoItemsWithPaginationQuery request, CancellationToken cancellationToken) => 
+                ([AsParameters] GetTodoItemsWithPaginationRequest request, CancellationToken cancellationToken) => 
                     HandleAsync(request, cancellationToken))
             .WithDescription("Used to get a list of todos")
+            .WithOpenApi(ops =>
+            {
+                ops.Parameters[0].Description = "The current page number. The first page is 1";
+                ops.Parameters[1].Description = "The number of records on a page";
+
+                return ops;
+            })
             .Produces<PaginatedListResponse<GetToDoItemsResponse>>(StatusCodes.Status200OK);
     }
 
-    public async Task<PaginatedListResponse<GetToDoItemsResponse>> HandleAsync(GetTodoItemsWithPaginationQuery request, CancellationToken cancellationToken)
+    public async Task<PaginatedListResponse<GetToDoItemsResponse>> HandleAsync(GetTodoItemsWithPaginationRequest request, CancellationToken cancellationToken)
     {
         var query = _mapper.Map<GetTodoItemsWithPaginationQuery>(request);
 
---
+++ src/MinimalApiTemplate.Api/Infrastructure/CustomExceptionHandler.cs
@@ -0,0 +1,172 @@
+﻿using MinimalApiTemplate.Application.Common.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace MinimalApiTemplate.Api.Infrastructure;
+
+public class CustomExceptionHandler : IExceptionHandler
+{
+    private readonly Dictionary<Type, Func<HttpContext, Exception, CancellationToken, Task>> _exceptionHandlers;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    private readonly ILogger<CustomExceptionHandler> _logger;
+
+    public CustomExceptionHandler(IWebHostEnvironment webHostEnvironment,
+        ILogger<CustomExceptionHandler> logger)
+    {
+        // Register known exception types and handlers.
+        _exceptionHandlers = new Dictionary<Type, Func<HttpContext, Exception, CancellationToken, Task>>
+        {
+            { typeof(ValidationException), HandleValidationException },
+            { typeof(NotFoundException), HandleNotFoundException },
+            { typeof(UnauthorizedAccessException), HandleUnauthorizedAccessException },
+            { typeof(ForbiddenAccessException), HandleForbiddenAccessException },
+            { typeof(BadRequestException), HandleBadRequestException },
+        };
+
+        _webHostEnvironment = webHostEnvironment;
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        await HandleException(httpContext, exception, cancellationToken);
+
+        return true;
+    }
+
+    private async Task HandleException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        Type type = exception.GetType();
+
+        if (_exceptionHandlers.ContainsKey(type))
+        {
+            await _exceptionHandlers[type].Invoke(context, exception, cancellationToken);
+            return;
+        }
+
+        await HandleUnhandledException(context, exception, cancellationToken);
+    }
+
+    private async Task HandleValidationException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        var validatoinException = (ValidationException)exception;
+
+        var details = new ValidationProblemDetails(validatoinException.Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.BadRequest, details, cancellationToken);
+
+   }
+
+    private async Task HandleUnhandledException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Unhandled exception");
+
+        var errorDetail = "An error occurred while processing your request.";
+
+        if (!_webHostEnvironment.IsProduction() && !_webHostEnvironment.IsStaging())
+        {
+            errorDetail = exception.GetFullErrorMessage();
+        }        
+
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Internal Server Error",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+            Detail = errorDetail
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.InternalServerError, details, cancellationToken);
+    }
+
+    private async Task HandleNotFoundException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        var notFoundexception = (NotFoundException)exception;
+
+        var details = new ProblemDetails()
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "The specified resource was not found.",
+            Detail = notFoundexception.Message
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.NotFound, details, cancellationToken);
+    }
+
+    private async Task HandleBadRequestException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        var badRequestException = (BadRequestException)exception;
+
+        var details = new ProblemDetails()
+        {
+            Type = "https://www.rfc-editor.org/rfc/rfc7231#section-6.5.1",
+            Title = "Bad Request",
+            Detail = badRequestException.Message
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.BadRequest, details, cancellationToken);
+    }
+
+    private async Task HandleUnauthorizedAccessException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status401Unauthorized,
+            Title = "Unauthorized",
+            Type = "https://tools.ietf.org/html/rfc7235#section-3.1"
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.Unauthorized, details, cancellationToken);
+    }
+
+    private async Task HandleForbiddenAccessException(
+        HttpContext context, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        var details = new ProblemDetails
+        {
+            Status = StatusCodes.Status403Forbidden,
+            Title = "Forbidden",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3"
+        };
+
+        await WriteErrorMessageToContext(context, HttpStatusCode.Forbidden, details, cancellationToken);
+    }
+    
+    private static async Task WriteErrorMessageToContext<T>(
+        HttpContext context, 
+        HttpStatusCode httpStatusCode, 
+        T problemDetails,
+        CancellationToken cancellationToken) where T : ProblemDetails
+    {
+        context.Response.StatusCode = (int)httpStatusCode;
+
+        await context.Response.WriteAsJsonAsync<T>(problemDetails, cancellationToken);
+    }
+}
