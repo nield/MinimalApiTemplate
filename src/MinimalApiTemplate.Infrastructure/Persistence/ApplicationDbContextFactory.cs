c69a8d92728ef748fec667361ed596372439b4c7
Niel de Wet <nieldw@gobalkinetic.com>
2025-02-10 14:58:33 +0200

    fix: migrations

---
+++ src/MinimalApiTemplate.Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MinimalApiTemplate.Infrastructure.Persistence;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        var config = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+        optionsBuilder.UseSqlServer(
+            config.GetConnectionString("SqlDatabase"),
+            builder =>
+                builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)
+                    .MigrationsHistoryTable(ApplicationDbContext.MigrationTableName, ApplicationDbContext.DbSchema));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
